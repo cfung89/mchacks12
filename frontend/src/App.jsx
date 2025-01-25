2d78983160a081ae8f8c06c956df56a9d39bd69e
cw118 <carolyn94118@gmail.com>
2025-01-25 15:59:51 -0500

    chore(frontend): start task component, react icons dep

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import Task from "./components/Task";
+
 function App() {
   return (
     <>
       <h1>Toodles!</h1>
+      <div className="container p-4">
+        <Task
+          taskName="Toodle!"
+          xp={10}
+          content="Toodle doodle doo, toodle doodle doo."
+          tag="toodly"
+        />
+      </div>
     </>
   );
 }
--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BsCheckCircleFill } from "react-icons/bs";
 
-const Task = () => {
-  return <div>Task</div>;
+const Task = ({ taskName, xp, content, tag }) => {
+  return (
+    <div className="task bg-pastel-blue px-8 py-6 rounded-md">
+      <h2 className="taskName text-xl font-semibold">{taskName}</h2>
+      <h3 className="taskXp">{xp} XP</h3>
+      <p className="taskContent">{content}</p>
+      <div className="taskFooter flex justify-between">
+        <p className="taskTag">#{tag}</p>
+        <div className="taskCheckbox">
+          <BsCheckCircleFill />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Task;
